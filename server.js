a30292099e3fe40e56e25eea9159943ee4e4d662
Shahar-Kabesa <61517666+shaharkabesa@users.noreply.github.com>
2026-02-07 08:22:50 +0200

    Working on backend

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const cors = require('cors');
+
+
+const app = express();
+
+const PORT = 3000
+
+app.use(cors({
+    origin: "*"
+}));
+app.use(express.json);
+
+
+let mockData = [ {
+    "id": 1,
+    "Author": "Shahar",
+    "Details": "This is the first blog mockup made by the creator shahar"
+    },
+    {
+        "id": 2,
+        "Author": "Lahan",
+        "Details": "This is the second blog post made by the creator girlfriend"
+    }
+];
+
+app.post('AddBlog', (req, res)=> {
+    
+})
+
+
+app.get('/data', (req,res) => {
+    console.log(req);
+    res.json(mockData);
+}) 
+
+app.listen(PORT, () => {
+    console.log(`Server is running on localhost at port:${PORT}`);
+})
